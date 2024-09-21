59f6e06d14064870587c31f3ee347f8f70a51c23
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-09-21 11:30:53 +0300

    Update rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,17 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
+
 import eventReducer from './event/events-slice';
 import filterReducer from './filter/filter-slice';
 
 
 
-const persistConfig={
-  key:"root",
-  storage,
-  whitelist:["token"]
-}
-const persistedAuthReducer=persistReducer(persistConfig);
 
 const rootReducer = combineReducers({
   event: eventReducer,
